7e6475757b08a8871399588013b2f1737a6f7cbd
Roger Clotet <roger.clotet@socialpoint.es>
2017-01-07 15:51:26 +0100

    Fighting the Zombie

--- 3_fightingthezombie/zombie.go
+++ 3_fightingthezombie/zombie.go
@@ -16,13 +16,13 @@ func main() {
 func Run(r io.Reader) []string {
 	results := []string{}
 	for i, c := range parseInput(r) {
-		result := fmt.Sprintf("Case #%d: %.06f", i+1, c.probabilityOfDefeat())
+		result := fmt.Sprintf("Case #%d: %.06f", i+1, c.maxProbabilityOfKill())
 		results = append(results, result)
 	}
 	return results
 }
 
-func parseInput(r io.Reader) []inputCase3 {
+func parseInput(r io.Reader) []inputCase {
 	var (
 		t int
 		s int
@@ -30,18 +30,18 @@ func parseInput(r io.Reader) []inputCase3 {
 
 	fmt.Fscanln(r, &t)
 
-	cases := []inputCase3{}
+	cases := []inputCase{}
 	for i := 0; i < t; i++ {
-		c := inputCase3{
-			rolls: []diceRoll{},
+		c := inputCase{
+			rolls: []DiceRoll{},
 		}
-		fmt.Fscan(r, &c.h, &s)
+		fmt.Fscan(r, &c.hp, &s)
 
 		for j := 0; j < s; j++ {
-			roll := diceRoll{}
+			roll := DiceRoll{}
 			var rollString string
 			fmt.Fscan(r, &rollString)
-			fmt.Sscanf(rollString, "%dd%d%d", &roll.dice, &roll.sides, &roll.offset)
+			fmt.Sscanf(rollString, "%dd%d%d", &roll.Dice, &roll.Faces, &roll.Offset)
 			c.rolls = append(c.rolls, roll)
 		}
 
@@ -51,17 +51,42 @@ func parseInput(r io.Reader) []inputCase3 {
 	return cases
 }
 
-type diceRoll struct {
-	dice   int
-	sides  int
-	offset int
+type inputCase struct {
+	hp    int
+	rolls []DiceRoll
 }
 
-type inputCase3 struct {
-	h     int
-	rolls []diceRoll
+func (c inputCase) maxProbabilityOfKill() float64 {
+	var probability float64
+	for _, roll := range c.rolls {
+		rp := roll.ProbabilityOfKill(c.hp - roll.Offset)
+		if rp > probability {
+			probability = rp
+		}
+	}
+	return probability
 }
 
-func (c inputCase3) probabilityOfDefeat() float64 {
-	return 0
+type DiceRoll struct {
+	Dice   int
+	Faces  int
+	Offset int
+}
+
+func (d DiceRoll) ProbabilityOfKill(zombieHP int) float64 {
+	if zombieHP <= 0 {
+		return 1
+	} else if d.Dice < 1 {
+		return 0
+	}
+
+	var probability float64
+
+	d.Dice--
+	factor := 1 / float64(d.Faces)
+	for damage := 1; damage <= d.Faces; damage++ {
+		probability += d.ProbabilityOfKill(zombieHP - damage) * factor
+	}
+
+	return probability
 }
